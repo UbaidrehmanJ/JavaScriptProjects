550acd5601a36de730eddd0932733a3fdc1aa635
UbaidrehmanJ <65415407+UbaidrehmanJ@users.noreply.github.com>
2020-10-24 13:15:10 +0500

    Update script.js

--- p09/script.js
+++ p09/script.js
@@ -16,6 +16,59 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+function generateID() {
+    return Math.floor(Math.random() * 10000000)
+}
+/*
+function addTransaction(e) {
+    e.preventDefault();
+
+    if( description.value.trim() === '' || amount.value.trim() === '' ) {
+        alert('Please enter a valid description and transaction amount.')
+    } else {
+        const transaction = {
+            id: generateID(),
+            description: description.value,
+            amount: +amount.value
+            };
+        
+        transactions.push(transaction);
+
+        addTransactionUI(transaction);
+        updateSums();
+
+        description.value = '';
+        amount.value = '';
+    }
+}
+
+*/
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (description.value.trim() === '' || (amount.value.trim() === "")) {
+        alert("Please enter valid value")
+    } else {
+        const transaction = {
+            id: generateID(),
+            description: description.value,
+            amount: +amount.value
+        };
+    transactions.push(transaction);
+
+    addTransactionUI(transaction);
+    updateSums();
+
+    description.value = " ";
+    amount.value = " ";
+
+        
+    }
+
+    
+}
+
+
 function addTransactionUI(transactions) {
     const type = transactions.amount > 0 ? '+' : '-';
 
@@ -24,18 +77,58 @@ function addTransactionUI(transactions) {
     item.innerHTML = ` 
         ${transactions.description}
         <span>${type} ${Math.abs(transactions.amount)}</span>
-        <button class="delete-btn" id="delete-btn">X</button>
+        <button class="delete-btn">X</button>
     `;
 
     list.appendChild(item);
 
+}
 
+// Function to Remove a Transaction
+/*function deleteTransaction(id) {
+    transactions = transactions.filter( transaction => transaction.id != id );
+    init();
+}*/
+
+function updateSums() {
+    const amount = transactions.map( transaction => transaction.amount );
+    const total = amount.reduce( (acc, item) => (
+        acc += item
+    ), 0)
+    .toFixed(2);
+
+
+    
+    const totalIncome = amount.filter( item => item > 0
+    )
+    .reduce( (acc, item) => (
+        acc += item
+    ), 0)
+    .toFixed(2);
+
+    
+    const totalExpense = amount.filter( item => item < 0
+    )
+    .reduce( (acc, item) => (
+        acc += item
+    ), 0)
+    .toFixed(2);
+    
+
+
+    balance.innerText = ` ${total}`;
+    moneyPlus.innerText = `${totalIncome}`;
+    moneyMinus.innerText = `${totalExpense}`;
 
 }
 
 function init() {
     transactions.forEach(addTransactionUI);
 
+    updateSums();
+
 }
+
+form.addEventListener('submit', addTransaction);
 
 init();
